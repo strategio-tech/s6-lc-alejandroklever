e343ca55ede40abd3bbc59040e7afcaef15faaa3
Alejandro Klever <alejandroklever4197@gmail.com>
2022-12-09 14:33:07 -0500

    feat: complete the task

--- src/main/java/tech/strategio/LoanCalculator.java
+++ src/main/java/tech/strategio/LoanCalculator.java
@@ -3,17 +3,33 @@ package tech.strategio;
 import java.util.Scanner;
 
 public class LoanCalculator {
+    /**
+     * Given a loan each month a 10% of the remaining loan are going to be paid.
+     * This method calculate the remaining loan after n month of payments.
+     *
+     * @param: loan amount
+     * @param: number of month that are going to be paid
+     * @return: remaining due amount after the given number of month
+     */
+    static int getRemainingAmountInNMonths(int amount, int numberOfMonths) {
+        var remainingDue = amount;
+
+        for (int i = 0; i < numberOfMonths; i++) {
+            remainingDue -= 10 * remainingDue / 100;
+        }
+
+        return remainingDue;
+    }
 
     /**
-     * TODO: finish out this JavaDoc comment block.
-     * FIXME: WHAT DOES THIS METHOD DO?
-     * 
-     * @param ???
-     * @return ???
+     * Given a loan each month a 10% of the remaining loan are going to be paid.
+     * This method calculate the remaining loan after 3 month of payments.
+     *
+     * @param: loan amount
+     * @return: remaining due amount after 3 month
      */
     static int getRemainingAmountIn3Months(int amount) {
-        // TODO: Rewrite this method
-        return -1;
+        return getRemainingAmountInNMonths(amount, 3);
     }
 
     public static void main(String[] args) {
